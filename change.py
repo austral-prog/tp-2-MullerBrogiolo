160425e2379223d03fff0302093e58644cf703b5
MullerBrogiolo <atbrogiolo@mail.austral.edu.ar>
2025-04-11 11:18:14 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,17 +1,23 @@
 def change():
     expense = 23.75
     money = 100
+    
+# Solicitar los datos de entrada al usuario
+    print("**Ingresar gasto:**")
+    gasto = float(input())  # Convertimos a float para manejar decimales
+    print("**Dinero recibido**")
+    dinero_recibido = float(input())
 
-# Calculo el vuelto
-    vuelto = money - expense
+# Calcular el vuelto total
+    vuelto = dinero_recibido - gasto
 
 # Separar pesos y centavos
-    pesos = int(vuelto)
-    centavos = round((vuelto - pesos) * 100)
+    pesos = int(vuelto)  # Obtenemos la parte entera
+    centavos = round((vuelto - pesos) * 100)  # Obtenemos los centavos redondeados
 
-# Imprimir el informe respetando el formato}
-    print("Vuelto")
-    print("Pesos:")
+# Mostrar el informe con el formato exacto requerido
+    print("\n**Vuelto**\n")  # \n para saltos de línea
+    print("**Pesos:**")
     print(pesos)
-    print("Centavos:")
+    print("\n**Centavos:**")
     print(centavos)
