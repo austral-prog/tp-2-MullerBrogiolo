4fdd71309120018f640bc69aa221b6ac6f084c58
MullerBrogiolo <atbrogiolo@mail.austral.edu.ar>
2025-04-11 10:45:04 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -2,3 +2,11 @@ def earth():
     x = "Bangladesh"
     y = "Barbados"
 
+    X_esta_antes = (X<Y)
+    Y_esta_antes = (Y<X)
+
+# Printeo los textos ya relacionados con las variables:
+
+    print(f"The result of {X} comes first in the dictionary than {Y} is {X_esta_antes}.")
+    print(f"The result of {Y} comes first in the dictionary than {X} is {Y_esta_antes}.")
+
