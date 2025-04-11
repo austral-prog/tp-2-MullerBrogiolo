046373109624d4e3fcff724cc29ac3a64622e09a
MullerBrogiolo <atbrogiolo@mail.austral.edu.ar>
2025-04-11 10:42:29 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,20 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    # Solicito los datos al usuario
+    costo = float(input("Me sale "))
+    pago = float(input("Le entregué "))
+
+# Calculo el vuelto total
+    vuelto = pago - costo
+
+# Separo en pesos y centavos
+    pesos = int(vuelto)
+    centavos = int(round((vuelto - pesos) * 100))
+
+# Imprimo la respuesta:
+    print("Me deben dar de vuelto:")
+    print("Pesos:")
+    print(pesos)
+    print("Centavos:")
+    print(centavos)
