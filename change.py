32e41ecbbe8e8db56e22e77a974374684fbbf514
MullerBrogiolo <atbrogiolo@mail.austral.edu.ar>
2025-04-11 10:41:29 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,17 @@
 def change():
     expense = 23.75
     money = 100
+    # Defino las variables: 
+
+    X = "Bangladesh"
+    Y = "Barbados"
+
+# Defino las variables:
+
+    X_esta_antes = (X<Y)
+    Y_esta_antes = (Y<X)
+
+# Printeo los textos ya relacionados con las variables:
+
+    print(f"The result of {X} comes first in the dictionary than {Y} is {X_esta_antes}.")
+    print(f"The result of {Y} comes first in the dictionary than {X} is {Y_esta_antes}.")
