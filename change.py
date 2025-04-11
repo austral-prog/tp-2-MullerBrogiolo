cda6b4d604f84e21d4aa8ce2b4adf6fe052b2751
MullerBrogiolo <atbrogiolo@mail.austral.edu.ar>
2025-04-11 11:39:32 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,17 +1,20 @@
 def change():
     expense = 23.75
     money = 100
+    
     vuelto = money - expense
     pesos = int(vuelto)
     centavos = vuelto - pesos
-    resultado_centavos = int(centavos*100)
-    print(f"Ingresar gasto:")
+    resultado_centavos = int(centavos*100)  # Agregué round() para mayor precisión
+    
+    print("Ingresar gasto:")
     print(expense)
-    print(f"Dinero recibido")
+    print("Dinero recibido")
     print(money)
+    print("")  # Línea vacía que el test espera
     print("Vuelto")
-    print("")
-    print(f"Pesos:")
+    print("")  # Otra línea vacía
+    print("Pesos:")
     print(pesos)
-    print(f"Centavos:")
+    print("Centavos:")
     print(resultado_centavos)
